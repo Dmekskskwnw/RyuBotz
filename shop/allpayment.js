9936bc1cd3eae9bed27283ea96d37a264612ca84
Dmekskskwnw <96872278+Dmekskskwnw@users.noreply.github.com>
2022-02-04 19:45:15 +0800

    Add files via upload

---
+++ shop/allpayment.js
@@ -0,0 +1,28 @@
+const allpayment = (prefix) => {
+return `┏━━━━━━━━━━━━━━━━━━━━
+┃           *PAYMENT*
+┣━━━━━━━━━━━━━━━━━━━━
+┣━━⊱❉ *💰 E-WALLET* ❉⊰━━━✿
+┃   
+┣━⊱ *GOPAY*
+┣⊱ 082322826333
+┣━━✿ NAMA GOPAY ${Fahri}
+┃
+┣━⊱ *DANA*
+┣⊱ 0821958851040
+┣━━✿ NAMA DANA ${Fahri}
+┃
+┣━⊱ *PULSA*
+┣⊱ 0821958851040
+┣━━✿ NAMA PULSA ${Fahri}
+┃
+┗━━━━━━━━━━━━━━━━━━━━
+
+Kirim Foto Bukti Tf Menggunakan !donetf
+
+NOTE! : Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
